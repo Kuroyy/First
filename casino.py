b4f2d4da1bfec80c4775a3b9018fde50c89211c0
Kuroyy <91373252+Kuroyy@users.noreply.github.com>
2021-10-06 19:38:36 +0300

    Add files via upload

--- casino.py
+++ casino.py
@@ -1,5 +1,6 @@
 import random
 
+# TODO сделать арену и инвентарь для Муромца
 
 def play_dice(bet):
     result = 0
@@ -16,7 +17,7 @@ def play_dice(bet):
 user_money = 5000
 print(f"У вас в начале {user_money} денег")
 
-for i in range(3):
-    user_money += play_dice(500)  # +500 или -500
+for i in range(5000):
+    user_money += play_dice(5000)  # +500 или -500
 
 print(f"У вас в финале {user_money} денег")
