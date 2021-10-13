0a663ea4a0de58fb746fceab91c2d3e40d590534
Kuroyy <91373252+Kuroyy@users.noreply.github.com>
2021-10-13 19:32:59 +0300

    Add files via upload

---
+++ tortoise.py
@@ -0,0 +1,34 @@
+# https://digitology.tech/docs/python_3/library/turtle.html
+import turtle as t
+
+
+def draw_square(side_lenght, x, y, line_color, fill_color):
+    # настройка
+    t.pencolor(line_color)
+    t.fillcolor(fill_color)
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+    # рисование
+    t.begin_fill()
+    t.forward(side_lenght)
+    t.left(90)
+    t.forward(side_lenght)
+    t.left(90)
+    t.forward(side_lenght)
+    t.left(90)
+    t.forward(side_lenght)
+    t.left(90)
+    t.end_fill()
+
+# Настройка
+t.shape("turtle")
+t.speed(1)
+
+
+# рисование
+draw_square(100, 0, 0, "#ff0000", "#0000ff")
+
+
+t.done()
